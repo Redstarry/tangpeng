199a8ab30d5e9e7258965ce94049036d62bbd8fe
tp <421363447@qq.com>
2019-04-17 10:45:26 +0800

    更改了九九乘法表的函数文档

--- Python案例/乘法表(函数版).py
+++ Python案例/乘法表(函数版).py
@@ -1,14 +1,14 @@
 #!/user/bin/env python3
-'''
-    乘法表的进阶函数版；
-    multiplication_table(number) 表示number以内的乘法表；
-    九九乘法表的时候，number = 10
-'''
+
 
 def MutiplicationTable(number):
+    '''
+    此函数是乘法表，可以实现任何位数的乘法表。
+    :param number: 这是一个数字，表示几以内的乘法表，例如：九九乘法表，number = 10；
+    :return:此函数没有返回值。
+    '''
     for x in range(1, number):
         for y in range(1, x + 1):
             print('%d * %d = %2d' % (y, x, x * y), end='  ')
         print('')
 
-MutiplicationTable(10)
